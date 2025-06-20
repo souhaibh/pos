0cb162d30d75bab4bdad30f2551193f8e8039c82
souhaibh <sousi.haydar1@gmail.com>
2025-06-20 16:33:18 +0300

    updatesss

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,11 @@
 const admin = require('firebase-admin');
 
-// Avoid reinitializing the app
 if (!admin.apps.length) {
   try {
     const serviceAccount = {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     };
 
     admin.initializeApp({
