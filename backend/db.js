d9b05e8c25dcb7b62999e15f77d06b69c5e9aba1
souhaibh <sousi.haydar1@gmail.com>
2025-06-20 16:36:18 +0300

    commit

--- backend/db.js
+++ backend/db.js
@@ -22,5 +22,7 @@ const db = admin.firestore();
 console.log('Firebase Project ID:', process.env.FIREBASE_PROJECT_ID);
 console.log('Firebase Client Email:', process.env.FIREBASE_CLIENT_EMAIL);
 console.log('Firebase Private Key starts with:', process.env.FIREBASE_PRIVATE_KEY?.substring(0, 20));
+console.log('FIREBASE_PRIVATE_KEY raw:', process.env.FIREBASE_PRIVATE_KEY);
+console.log('FIREBASE_PRIVATE_KEY parsed:', process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').slice(0, 50));
 
 module.exports = db;
